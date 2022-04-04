667b7f8e5bc5e1059140c0e732c27722b4aa4988
aymenfarhani <aymenfarhani28@gmail.com>
2022-04-04 20:09:05 +0100

    csv source and csv source file

--- src/test/java/junit5tests/ParameterizedTests.java
+++ src/test/java/junit5tests/ParameterizedTests.java
@@ -1,8 +1,9 @@
 package junit5tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ParameterizedTests {
@@ -12,5 +13,29 @@ public class ParameterizedTests {
     @ValueSource(strings = { "Java Test", "Junit test"})
     void lengthOfString(String name){
         System.out.println("The length of String parameter is " + name.length());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"junit, test", "java, spring", "spring batch, spring boot"})
+    void csvSource_String_String(String output1, String output2){
+        System.out.println("Output 1 = " + output1 + " , Output 2 = " + output2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"junit, 32, true", "java, 15, false", "spring batch, 10, true"})
+    void csvSource_String_Int_Boolean(String output1, int output2, boolean output3){
+        System.out.println("Output 1 = " + output1 + " , Output 2 = " + output2+ " , Output 3 = " + output3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"junit?test", "java?spring", "spring batch?spring boot"}, delimiter = '?')
+    void csvSource_String_String_WithDelimiter(String output1, String output2){
+        System.out.println("Output 1 = " + output1 + " , Output 2 = " + output2);
+    }
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/params/shoppinglist.csv", numLinesToSkip = 1)
+    void csvSource_String_String_FromFile(String name, double price, int quantity, String uom, String provider){
+        System.out.println("name = " + name + " , price = " + price + " , quantity = " + quantity
+                + " , uom = " + uom + " , provider = " + provider);
     }
 }
