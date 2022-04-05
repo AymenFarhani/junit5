2ae06dff53b67ca10333c74bdfc30f6724904a01
aymenfarhani <aymenfarhani28@gmail.com>
2022-04-05 17:28:30 +0100

    hamecrast

--- src/test/java/junit5tests/AssertionsTest.java
+++ src/test/java/junit5tests/AssertionsTest.java
@@ -1,10 +1,14 @@
 package junit5tests;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -27,5 +31,15 @@ public class AssertionsTest {
         int[] expectedValues = {1,2,3};
         int[] actualValues = {1,2,3};
         assertArrayEquals(expectedValues, actualValues);
+    }
+
+    @Test
+    void assertForMapTest() {
+        Map<String, Integer> mapData = new HashMap<>();
+        mapData.put("First data", 1);
+        mapData.put("Second String",2);
+        mapData.put("Third String",3);
+        assertThat(mapData, Matchers.hasValue(2));
+        assertThat(mapData, Matchers.hasKey("Second String"));
     }
 }
